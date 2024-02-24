4e47c10913bfebeaeb382313cc09471338d7ceff
Christian Romney <christian.a.romney@gmail.com>
2024-02-24 11:15:51 -0500

    Clean up and reorganize notes and implementation
    Signed-off-by: Christian Romney <christian.a.romney@gmail.com>

---
+++ agent.py
@@ -0,0 +1,43 @@
+from llama_index.core.tools import FunctionTool, QueryEngineTool, ToolMetadata
+from llama_index.core.agent import ReActAgent
+from llama_index.llms.ollama import Ollama
+from llama_index.core import Settings
+import pytz
+
+# Agent Tools
+def current_date() -> str:
+  "Gets the current time as a string."
+  return datetime.now(pytz.timezone("US/Eastern")).strftime("%A, %B %d, %Y")
+
+def current_time() -> str:
+  "Gets the current time as a string."
+  return datetime.now(pytz.timezone("US/Eastern")).strftime("%I:%M%p %Z")
+
+llm = Ollama(model="mistral", request_timeout=60.0)
+query_engine = None # TODO: this is not working yet
+
+toolbox = [
+  FunctionTool.from_defaults(
+    fn=current_date,
+    name="current_date",
+    description=(
+      "Returns today's date."
+    )
+  ),
+  FunctionTool.from_defaults(
+    fn=current_time,
+    name="current_time",
+    description=(
+      "Returns the current time."
+    )
+  ),
+  QueryEngineTool.from_defaults(
+    query_engine=query_engine,
+    name="my_notes",
+    description=(
+      "Returns information about my personal org-mode notes and todo list items."
+      "Use a detailed plain text question as input to the tool."
+    )
+  )
+]
+agent = ReActAgent.from_tools(toolbox, llm=Settings.llm, verbose=True)
--- rag.py
+++ rag.py
@@ -1,87 +1,115 @@
-from deepeval.integrations.llama_index import DeepEvalContextualRelevancyEvaluator
-from llama_index.core import VectorStoreIndex, SimpleDirectoryReader, Settings
-from llama_index.core import Document, StorageContext, load_index_from_storage
+from llama_index.core import VectorStoreIndex, SimpleDirectoryReader
+from llama_index.core import StorageContext, load_index_from_storage
+from llama_index.core import Settings, get_response_synthesizer
+from llama_index.core.retrievers import VectorIndexRetriever
+from llama_index.core.postprocessor import SimilarityPostprocessor
+from llama_index.core.postprocessor import FixedRecencyPostprocessor
+from llama_index.core.query_engine import CitationQueryEngine
+from llama_index.core.chat_engine.condense_question import CondenseQuestionChatEngine
 from llama_index.core.embeddings import resolve_embed_model
 from llama_index.llms.ollama import Ollama
-import os, getopt, sys
+import os, getopt, sys, shutil
 
+# global settings
 Settings.embed_model = resolve_embed_model("local:BAAI/bge-small-en-v1.5")
-Settings.llm = Ollama(model="gemma", request_timeout=30.0)
+Settings.llm = Ollama(model="mistral", request_timeout=60.0)
 
+verbose=False
 def log(message):
   "Prints a message iff verbose is True"
   if verbose:
     print(message)
 
 class DocumentIndex:
-  def __init__(self, directory, progress=True, verbose=False):
+  def __init__(self, directory, exts=[".org"], progress=True,
+               verbose=False, max_top_k=10, top_k=5,
+               similarity_cutoff=0.6):
+    # load or create the index
     self.path = os.path.join(directory, ".llamaindex")
-    self.documents = SimpleDirectoryReader(input_dir=directory,
-                                           recursive=True,
-                                           required_exts=[".org"]).load_data()
     if os.path.exists(self.path):
       context= StorageContext.from_defaults(persist_dir=self.path)
       log("Loading index from disk")
       self.index = load_index_from_storage(context)
     else:
       log("Creating new index")
-      self.index = VectorStoreIndex.from_documents(self.documents,
-                                                   show_progress=progress)
-      self.save()
+      docs = SimpleDirectoryReader(input_dir=directory,
+                                   recursive=True,
+                                   required_exts=exts).load_data()
+      log(f"Read {len(docs)} {', '.join(exts)} docs from {directory}.")
+      index = VectorStoreIndex.from_documents(docs, show_progress=progress)
+      index.storage_context.persist(persist_dir=self.path)
+      self.index = index
 
-  def refresh(self):
-    "Refreshes the index from the updated documents and saves to disk."
-    log("Refreshing index with changed documents")
-    self.index.refresh(self.documents)
-    self.save()
+    # post-processors filter the nodes returned from the similarity search
+    # prior to creating the context for the LLM call
+    self.post_processors = [
+      SimilarityPostprocessor(similarity_cutoff=similarity_cutoff),
+      FixedRecencyPostprocessor(top_k=top_k, date_key='last_modified_date')
+    ]
 
-  def save(self):
-    "Saves the index to disk under the given directory."
-    log("Saving index to disk")
-    self.index.storage_context.persist(persist_dir=self.path)
+    # compact and refine synthesizer
+    self.response_synth = get_response_synthesizer()
+
+    self.query_engine = CitationQueryEngine(
+      retriever=VectorIndexRetriever(
+        index=self.index, similarity_top_k=top_k,
+        max_top_k=max_top_k),
+      response_synthesizer=self.response_synth,
+      node_postprocessors=self.post_processors
+    )
+    self.chat_engine = CondenseQuestionChatEngine.from_defaults(
+      query_engine=self.query_engine
+    )
+
+  def delete_index(self):
+    "Deletes the index folder and all files"
+    shutil.rmtree(self.path)
 
   def print_files(self):
     "Prints the list of all files in the index."
     files = [info.metadata["file_path"] for info in self.index.ref_doc_info.values()]
     print("\n".join(files))
 
-  def query(self, q, evaluate_result=False):
-    "Returns the response to the given query."
-    result = self.index.as_query_engine().query(q)
-    print(result)
-    if not evaluate_result:
-      return {'response': result}
-    else:
-      judge = DeepEvalContextualRelevancyEvaluator()
-      evaluation = judge.evaluate_response(query=q, response=result)
+  def _print_retrieved_item(self, idx, item):
+    print(f"{idx}. file: {item.node.metadata['file_path']}")
+    print(f"   size: {item.node.metadata['file_size']}")
+    print(f"   score: {round(item.score, 3)}")
+    print(f"   embedding: {item.node.embedding}")
 
-      print(f"""
-DeepEval Contextual Relevancy
------------------------------
-Score: {evaluation.score}
-Contexts: {evaluation.contexts}
-Explanation:
-{evaluation.feedback}
-      """)
-      return {'response': result, 'evaluation': evaluation}
+  def _print_retrieved_items(self, items):
+    heading = f"\nRetrieved {len(items)} Nodes"
+    dashes = "-" * len(heading)
+    print(f"{heading}\n{dashes}")
+    for i in range(len(items)):
+      self._print_retrieved_item(i + 1, items[i])
+
+  def print_retrieved(self, q):
+    "Print nodes retrieved from the index."
+    items = self.query_engine.retriever.retrieve(q)
+    self._print_retrieved_items(items)
+
+  def query(self, q, evaluate_result=False):
+    "Prints the response to the given query."
+    print(self.query_engine.query(q))
 
   def chat(self, mode="context", stream=True):
-    self.index.as_chat_engine(chat_mode=mode,
-                              streaming=stream).streaming_chat_repl()
+    "Starts a chat repl."
+    self.chat_engine.streaming_chat_repl()
 
 if __name__ == "__main__":
   # default values
-  verbose = False
   interactive = False
-  refresh = False
+  print_retrieved = False
   listing = False
   evaluate = False
   query = ''
   directory = "/Users/christian/Documents/personal/notes/content/"
 
+  # argument parsing
   arguments = sys.argv[1:]
-  short_opts = 'virleq:d:'
-  long_opts = ['verbose', 'interactive', 'refresh', 'list', 'eval', 'query=', 'directory=']
+  short_opts = 'vipleq:d:'
+  long_opts = ['verbose', 'interactive', 'print-retrieved',
+               'list', 'eval', 'query=', 'directory=']
 
   try:
     opts, _args = getopt.getopt(arguments, short_opts, long_opts)
@@ -90,8 +118,8 @@ if __name__ == "__main__":
         verbose = True
       elif opt in ('-i', '--interactive'):
         interactive = True
-      elif opt in ('-r', '--refresh'):
-        refresh = True
+      elif opt in ('-p', '--print-retrieved'):
+        print_retrieved = True
       elif opt in ('-l', '--list'):
         listing = True
       elif opt in ('-e', '--eval'):
@@ -101,18 +129,18 @@ if __name__ == "__main__":
       elif opt in ('-d', '--directory'):
         directory = arg
 
+    # RAG class
     index = DocumentIndex(directory)
+
+    # dispatch action
     if listing:
       index.print_files()
     elif interactive:
-      index.chat()
+      agent.chat()
     elif query:
       index.query(query, evaluate_result=evaluate)
-
-    if refresh:
-      index.refresh()
-
-    log("Goodbye.")
+      if print_retrieved:
+        index.print_retrieved(query)
   except getopt.GetoptError as err:
     print(str(err))
     sys.exit(2)
